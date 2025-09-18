ac99332e01ed6cdee3635dfeff19c8cb8835d3b2
samith-s <samith.s@intervest.lk>
2025-09-18 13:21:29 +0530

    variables-and-data-types

---
+++ README.md
@@ -0,0 +1,2 @@
+Variables;
+There are three types of variable declarations in JavaScript: var, let, and const. Each has its own characteristics and use cases.
---
+++ variables-and-data-types/index.js
@@ -0,0 +1,8 @@
+var name = "Samith";
+let name = "Sample name";
+const name = "Another name";
+
+let number = 10;
+let isActive = true;
+let score = null;
+let address = undefined;
