06a8108306f0871c0506e5916f363a1a68774fb0
samith-ws <samithsankalpa67@gmail.com>
2025-09-19 22:08:10 +0530

    loops and switch case

---
+++ if-conditions/switch.js
@@ -0,0 +1,12 @@
+let month = "dd";
+
+switch (month) {
+  case "January":
+    console.log("It's January");
+    break;
+  case "February":
+    console.log("It's February");
+    break;
+  default:
+    console.log("It's not January or February");
+}
