36c448506b32c097ccf51334d36e9477f31561df
samith-ws <samithsankalpa67@gmail.com>
2025-09-19 22:42:13 +0530

    functions and arrow functions

---
+++ functions/function.js
@@ -0,0 +1,11 @@
+function getNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+let answer = getNumbers(5, 13);
+console.log(answer);
+
+//Arrow function
+const addNumber = (num1, num2) => {
+  return num1 + num2;
+};
