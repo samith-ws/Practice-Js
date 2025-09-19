53005309d63289dbe5824a66e443e2314cb6041f
samith-ws <samithsankalpa67@gmail.com>
2025-09-19 22:42:54 +0530

    functions and arrow functions (#7)

---
+++ loops-practice/index.js
@@ -0,0 +1,16 @@
+let myArray = [1, 2, 3, 4, 5];
+
+for (let index = 0; index < myArray.length; index++) {
+  //   console.log(myArray[index]);
+}
+
+let i = 0;
+
+while (i < 10) {
+  //   console.log(i);
+  i++;
+}
+
+for (let item of myArray) {
+  console.log(item);
+}
